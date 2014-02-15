ced5ed8fee3df0050ba22f39476e15fab2342be4
bouzuya <bouzuya@gmail.com>
2014-02-15 18:47:30 +0900

    add example "list_projects.js"

---
+++ examples/list_projects.js
@@ -0,0 +1,16 @@
+var util = require('util');
+var backlogApi = require('../');
+
+// $ export BACKLOG_SPACE_ID='backlog space id'
+// $ export BACKLOG_USERNAME='backlog username'
+// $ export BACKLOG_PASSWORD='backlog password'
+
+var backlog = backlogApi();
+backlog.getProjects().then(function(projects) {
+  projects.forEach(function(project) {
+    var format = '[%s] %s %s';
+    var s = util.format(format, project.key, project.name, project.url);
+    console.log(s);
+  });
+});
+
