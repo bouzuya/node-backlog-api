7bba80a27b5f5cd7e4846d313bcb008ed8584cae
bouzuya <m@bouzuya.net>
2018-01-23 00:12:58 +0900

    fix categories

--- test/_/fetch.ts
+++ test/_/fetch.ts
@@ -2,7 +2,7 @@ import { Test, test } from 'beater';
 import * as assert from 'power-assert';
 import { fetch } from '../../src/_/fetch';
 
-const category = '/fetch ';
+const category = '/_/fetch ';
 const tests: Test[] = [
   test(category + 'export fetch', () => {
     assert(typeof fetch === 'function');
--- test/_/get-base-url.ts
+++ test/_/get-base-url.ts
@@ -2,7 +2,7 @@ import { Test, test } from 'beater';
 import * as assert from 'power-assert';
 import { getBaseUrl } from '../../src/_/get-base-url';
 
-const category = '/get-base-url ';
+const category = '/_/get-base-url ';
 const tests: Test[] = [
   test(category, () => {
     assert(getBaseUrl('space') === 'https://space.backlog.jp');
